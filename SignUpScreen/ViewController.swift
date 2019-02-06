e6e23827b071d31cb986a1f2a104b2f846ef9835
Vahagn Gevorgyan <vahagngevorgyan@workfront.com>
2019-02-06 22:56:50 +0400

    Initial Commit

---
+++ SignUpScreen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SignUpScreen
+//
+//  Created by Vahgan Gevorgyan on 2/6/19.
+//  Copyright © 2019 Vahgan Gevorgyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
